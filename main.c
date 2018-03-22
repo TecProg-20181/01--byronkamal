72b0a38b7a6b674590bf6f19e65530cc175da7e3
byronkamal <byronkamal@hotmail.com>
2018-03-22 08:49:34 -0300

    Creating the method Espelhar

--- main.c
+++ main.c
@@ -151,6 +151,39 @@ for (unsigned int x = 0; x < img.height; ++x) {
     return img;
 }
 
+Image espelhar(Image img){
+    int horizontal = 0;
+    scanf("%d", &horizontal);
+
+    int widht = img.width, height = img.height;
+
+    if (horizontal == 1) widht /= 2;
+    else height /= 2;
+
+    for (int i2 = 0; i2 < height; ++i2) {
+      for (int j = 0; j < widht; ++j) {
+        int x = i2, y = j;
+
+        if (horizontal == 1) y = img.width- 1 - j;
+        else x = img.height - 1 - i2;
+
+        Pixel aux1;
+        aux1.red = img.pixel[i2][j][0];
+        aux1.green = img.pixel[i2][j][1];
+        aux1.blue = img.pixel[i2][j][2];
+
+        img.pixel[i2][j][0] = img.pixel[x][y][0];
+        img.pixel[i2][j][1] = img.pixel[x][y][1];
+        img.pixel[i2][j][2] = img.pixel[x][y][2];
+
+        img.pixel[x][y][0] = aux1.red;
+        img.pixel[x][y][1] = aux1.green;
+        img.pixel[x][y][2] = aux1.blue;
+      }
+    }
+    return img;
+ }
+
 Image read_pixel(Image img) {
 for (unsigned int i = 0; i < img.height; ++i) {
     for (unsigned int j = 0; j < img.width; ++j) {
@@ -159,9 +192,9 @@ for (unsigned int i = 0; i < img.height; ++i) {
                              &img.pixel[i][j][2]);
     }
   }
-
   return img;
 }
+
 
 int main() {
     Image img;
@@ -191,9 +224,9 @@ int main() {
             }
             case 2: { // Filtro Sepia
                 img = sepia(img);
+                break;
                 }
 
-                break;
             case 3: { // Blur
                 int tamanho = 0;
                 scanf("%d", &tamanho);
@@ -209,42 +242,17 @@ int main() {
                 }
                 break;
             }
+
             case 5: { // Espelhamento
-                int horizontal = 0;
-                scanf("%d", &horizontal);
+              img = espelhar(img);
+            break;
+          }
 
-                int width = img.width, height = img.height;
-
-                if (horizontal == 1) width /= 2;
-                else height /= 2;
-
-                for (int i2 = 0; i2 < height; ++i2) {
-                    for (int j = 0; j < width; ++j) {
-                        int x = i2, y = j;
-
-                        if (horizontal == 1) y = img.width - 1 - j;
-                        else x = img.height - 1 - i2;
-
-                        Pixel aux1;
-                        aux1.red = img.pixel[i2][j][0];
-                        aux1.green = img.pixel[i2][j][1];
-                        aux1.blue = img.pixel[i2][j][2];
-
-                        img.pixel[i2][j][0] = img.pixel[x][y][0];
-                        img.pixel[i2][j][1] = img.pixel[x][y][1];
-                        img.pixel[i2][j][2] = img.pixel[x][y][2];
-
-                        img.pixel[x][y][0] = aux1.red;
-                        img.pixel[x][y][1] = aux1.green;
-                        img.pixel[x][y][2] = aux1.blue;
-                    }
-                }
-                break;
-            }
             case 6: { // Inversao de Cores
                 inverter_cores(img.pixel, img.width, img.height);
                 break;
             }
+
             case 7: { // Cortar Imagem
                 int x, y;
                 scanf("%d %d", &x, &y);
