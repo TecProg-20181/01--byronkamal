8b8126b70ce78c0283c50abd8e58a9bbae57e518
Byron Kamal <byronkamal@hotmail.com>
2018-03-23 11:10:25 -0300

    Returning the funcition espelhar()

--- main.c
+++ main.c
@@ -130,6 +130,39 @@ for (unsigned int x = 0; x < img.height; ++x) {
     return img;
 }
 
+Image espelhar(Image img){
+      int horizontal = 0;
+     scanf("%d", &horizontal);
+
+    int widht = img.width, height = img.height;
+
+     if (horizontal == 1) widht /= 2;
+     else height /= 2;
+
+     for (int i2 = 0; i2 < height; ++i2) {
+       for (int j = 0; j < widht; ++j) {
+         int x = i2, y = j;
+
+         if (horizontal == 1) y = img.width- 1 - j;
+         else x = img.height - 1 - i2;
+
+         Pixel aux1;
+         aux1.red = img.pixel[i2][j][0];
+         aux1.green = img.pixel[i2][j][1];
+         aux1.blue = img.pixel[i2][j][2];
+
+         img.pixel[i2][j][0] = img.pixel[x][y][0];
+         img.pixel[i2][j][1] = img.pixel[x][y][1];
+         img.pixel[i2][j][2] = img.pixel[x][y][2];
+
+         img.pixel[x][y][0] = aux1.red;
+         img.pixel[x][y][1] = aux1.green;
+         img.pixel[x][y][2] = aux1.blue;
+       }
+     }
+     return img;
+  }
+
 
 Image read_pixel(Image img) {
 for (unsigned int i = 0; i < img.height; ++i) {
@@ -191,39 +224,8 @@ int main() {
             }
 
             case 5: { // Espelhamento
-              int horizontal = 0;
-              scanf("%d", &horizontal);
-            int horizontal;
-            horizontal = horizont;
-
-              int width = img.width, height = img.height;
-
-              if (horizontal == 1)
-              width /= 2;
-              else height /= 2;
-
-              for (int i2 = 0; i2 < height; ++i2) {
-                  for (int j = 0; j < width; ++j) {
-                      int x = i2, y = j;
-
-                      if (horizontal == 1) y = img.width - 1 - j;
-                      else x = img.height - 1 - i2;
-
-                      Pixel aux1;
-                      aux1.red = img.pixel[i2][j][0];
-                      aux1.green = img.pixel[i2][j][1];
-                      aux1.blue = img.pixel[i2][j][2];
-
-                      img.pixel[i2][j][0] = img.pixel[x][y][0];
-                      img.pixel[i2][j][1] = img.pixel[x][y][1];
-                      img.pixel[i2][j][2] = img.pixel[x][y][2];
-
-                      img.pixel[x][y][0] = aux1.red;
-                      img.pixel[x][y][1] = aux1.green;
-                      img.pixel[x][y][2] = aux1.blue;
-                  }
-            }
-            bre
+            img = espelhar(img);
+            break;
           }
 
             case 6: { // Inversao de Cores
